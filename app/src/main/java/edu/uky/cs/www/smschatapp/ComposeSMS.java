cc933eeba6942edd6e055105e7f9152de21f65ae
Bryan Potere <bpotere@gmail.com>
2015-03-19 19:39:29 -0400

    First commit.  A very rudimentary SMS application.

---
+++ app/src/main/java/edu/uky/cs/www/smschatapp/ComposeSMS.java
@@ -0,0 +1,71 @@
+package edu.uky.cs.www.smschatapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class ComposeSMS extends ActionBarActivity {
+
+    Button sendSMSBtn;
+    EditText toPhoneNumberET;
+    EditText smsMessageET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_sms);
+        sendSMSBtn = (Button) findViewById(R.id.sendSMSBtn);
+        toPhoneNumberET = (EditText) findViewById(R.id.toPhoneNumberET);
+        smsMessageET = (EditText) findViewById(R.id.smsMessageET);
+        sendSMSBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMS();
+            }
+        });
+    }
+
+    protected void sendSMS() {
+        String toPhoneNumber = toPhoneNumberET.getText().toString();
+        String smsMessage = smsMessageET.getText().toString();
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(toPhoneNumber, null, smsMessage, null, null);
+            Toast.makeText(getApplicationContext(), "SMS sent.",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(),
+                    "Sending SMS failed.",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_compose_sm, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
