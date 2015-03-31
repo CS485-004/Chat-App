a21f627d969c2fa33b21bcb79651d8371db226b7
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2015-03-30 21:12:41 -0400

    Added SettingsActivity

---
+++ app/src/main/java/edu/uky/cs/www/smschatapp/SettingsActivity.java
@@ -0,0 +1,32 @@
+package edu.uky.cs.www.smschatapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBarActivity;
+
+public class SettingsActivity extends ActionBarActivity {
+
+    // Keys as defined in settings.xml
+    public static String NOTIFICATIONS_KEY = "pref_notifications";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    /**
+     * SettingsFragment
+     * Builds a Preferences screen based on the content in settings.xml
+     */
+    public static class SettingsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+}
