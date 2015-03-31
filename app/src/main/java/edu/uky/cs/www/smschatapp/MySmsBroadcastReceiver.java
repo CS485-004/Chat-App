7eba6049592889d9a196867193a556cf81c6841d
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2015-03-30 21:13:56 -0400

    Added check for user's preference of displaying notification.

--- app/src/main/java/edu/uky/cs/www/smschatapp/MySmsBroadcastReceiver.java
+++ app/src/main/java/edu/uky/cs/www/smschatapp/MySmsBroadcastReceiver.java
@@ -7,7 +7,9 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.telephony.SmsMessage;
@@ -44,27 +46,35 @@ public class MySmsBroadcastReceiver extends BroadcastReceiver {
                 smsMessageStr += "SMS From: " + smsAddress + "\n";
                 smsMessageStr += smsBody + "\n";
             }
-            //Build the notification:
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                    .setSmallIcon(R.drawable.ic_action_chat)
-                    .setContentTitle("Message from " + smsAddress)
-                    .setContentText(smsBody);
 
-            Intent notificationIntent = new Intent(context, MainActivity.class);
-            // The stack builder object will contain an artificial back stack for the started Activity.
-            // This ensures that navigating backward from the Activity leads out of your application to the Home screen.
-            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-            // Adds the back stack for the Intent (but not the Intent itself)
-            stackBuilder.addParentStack(MainActivity.class);
-            // Adds the Intent that starts the Activity to the top of the stack
-            stackBuilder.addNextIntent(notificationIntent);
-            PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            //Get the user's settings
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            //Get the value for notifications
+            boolean notificationsOn = sharedPreferences.getBoolean(SettingsActivity.NOTIFICATIONS_KEY, true);
 
-            builder.setContentIntent(pendingIntent);
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notificationsOn) {
+                //Build the notification:
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_action_chat)
+                        .setContentTitle("Message from " + smsAddress)
+                        .setContentText(smsBody);
 
-            int id = 123; //needs to be an integer unique to this application to be able to update the notification later on after it's created. (or could create multiple notifications?)
-            notificationManager.notify(id, builder.build());
+                Intent notificationIntent = new Intent(context, MainActivity.class);
+                // The stack builder object will contain an artificial back stack for the started Activity.
+                // This ensures that navigating backward from the Activity leads out of your application to the Home screen.
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                // Adds the back stack for the Intent (but not the Intent itself)
+                stackBuilder.addParentStack(MainActivity.class);
+                // Adds the Intent that starts the Activity to the top of the stack
+                stackBuilder.addNextIntent(notificationIntent);
+                PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                builder.setContentIntent(pendingIntent);
+                NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                int id = 123; //TODO: needs to be an integer unique to this application to be able to update the notification later on after it's created. (or could create multiple notifications?)
+                notificationManager.notify(id, builder.build());
+            }
 
             Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
 
