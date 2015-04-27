7fe1a316e010d93d3e4934dfbff978bff70a3eba
Bryan Potere <bpotere@gmail.com>
2015-04-26 22:33:50 -0400

    Read messages is nearly done.

--- app/src/main/java/edu/uky/cs/www/smschatapp/read_messages.java
+++ app/src/main/java/edu/uky/cs/www/smschatapp/read_messages.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.provider.BaseColumns;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
@@ -52,14 +54,19 @@ public class read_messages extends ActionBarActivity {
 
     public void refreshSmsInbox() {
 
+        // Getting the contact number from the intent
         String contactNumber="";
         Intent intent = getIntent();
         contactNumber = intent.getStringExtra(TAG);
 
-        ContentResolver contentResolver = getContentResolver();
-        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        String[] qStr = new String[]{contactNumber};
+
+        // Getting all of the inbox messages
+        ContentResolver inboxResolver = getContentResolver();
+        Cursor smsInboxCursor = getContentResolver().query(Uri.parse("content://sms/inbox"), new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, "address=?", qStr, null);
         int indexBody = smsInboxCursor.getColumnIndex("body");
         int indexAddress = smsInboxCursor.getColumnIndex("address");
+        int indexDate = smsInboxCursor.getColumnIndex("date");
         if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
         arrayAdapter.clear();
 
@@ -67,16 +74,67 @@ public class read_messages extends ActionBarActivity {
         String contactName = getContactDisplayNameByNumber(contactNumber);
         displayName.setText(contactName);
 
-        do {
-            // Get the next number in the inbox
-            String thisNumber = smsInboxCursor.getString(indexAddress);
+        // Getting all of the sent sms messages
+        ContentResolver sentResolver = getContentResolver();
 
-            String str = "SMS From: " + contactName +
+        Cursor smsSentCursor = getContentResolver().query(Uri.parse("content://sms/sent"), new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, "address=?", qStr, null);;
+
+        boolean sentSMSExist = true;
+        long longSentDate = Long.MAX_VALUE;
+        String sentNumber, strSentDate, formattedSentDate;
+        if (!smsSentCursor.moveToFirst()){
+            sentSMSExist = false;
+        } else {
+            sentNumber = smsSentCursor.getString(indexAddress);
+            strSentDate = smsSentCursor.getString(indexDate);
+            longSentDate = Long.parseLong(strSentDate, 10);
+        }
+
+        long longGotDate;
+        String strGotNumber, strGotDate, formattedGotDate;
+        do {
+            //Start processing the sms messages gotten from the other number.
+            // Get the next number in the inbox
+            strGotNumber = smsInboxCursor.getString(indexAddress);
+            // Get the date sent
+            strGotDate = smsInboxCursor.getString(indexDate);
+            longGotDate = Long.parseLong(strGotDate, 10);
+            // Format that date
+            formattedGotDate = new SimpleDateFormat("MM/dd/yyyy").format(longSentDate);
+
+            // The message that will be visible
+            String str = contactName + " sent at " + formattedGotDate + ":" +
                     "\n" + smsInboxCursor.getString(indexBody) + "\n";
 
+            Log.v(TAG, "GOT: " + strGotNumber);
             // If this number is from the contact, add it to the list.
-            if(thisNumber.equals(contactNumber)){
+            if(longGotDate >= longSentDate){
                 arrayAdapter.add(str);
+                Log.v(TAG, "ADDED: " + str);
+            }
+            else {
+                boolean advance = false;
+                do {
+                    // Do the same thing to get the next sent message
+                    sentNumber = smsSentCursor.getString(indexAddress);
+                    strSentDate = smsSentCursor.getString(indexDate);
+                    longSentDate = Long.parseLong(strSentDate, 10);
+                    formattedSentDate = new SimpleDateFormat("MM/dd/yyy").format(longSentDate);
+                    Log.v(TAG, "SENT: " + sentNumber);
+
+                    String sentStr = "I sent at " + formattedSentDate + ":\n"
+                            + smsSentCursor.getString(indexBody) + "\n";
+                    advance = false;
+                    if (longGotDate < longSentDate){
+                        arrayAdapter.add(sentStr);
+                        Log.v(TAG, "ADDED: " + sentStr);
+                        advance = true;
+                    }
+                } while (advance && smsSentCursor.moveToNext() && longGotDate < longSentDate);
+
+                // Now add that last message that wasn't added before because longGotDate <= longSentDate
+                arrayAdapter.add(str);
+                Log.v(TAG, "ADDED: " + str);
             }
 
         } while (smsInboxCursor.moveToNext());
